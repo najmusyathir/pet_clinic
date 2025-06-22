3ef6820ccadbfa1b5e2dc603aba8692c543713b8
Najmu Syathir <134459622+najmusyathir@users.noreply.github.com>
2025-06-22 21:14:22 +0800

    Feature/users module n base UI (#2)
    * Update: welcom page ui
    
    * Update: Include migrations
    
    * Updare: Include assets and update button design
    
    * Modify: Include some notes
    
    * Modify: Update logo
    
    * Update: Include side navigation
    
    * Update: Include phone update features
    
    * Modify: Remove forgot password route
    
    * Modify: Fix autofill in register form
    
    * Update: User's module completed
    
    * Modify: Include new routes and update drawer

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,4 +18,10 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::middleware('auth')->group(function () {
+    Route::get('/user', [UserController::class, 'index'])->name('users');
+    Route::get('/profile/{id}', [UserController::class, 'detail'])->name('user.detail');
+    Route::put('/user/{id}/updateRole', [UserController::class, 'updateRole'])->name('user.updateRole');
+    Route::delete('/user/{id}/delete', [UserController::class, 'destroy'])->name('user.destroy');
+});
 require __DIR__.'/auth.php';
