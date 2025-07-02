0772363fef843ef14bdec70bd6eae839aed3b609
Najmu Syathir <134459622+najmusyathir@users.noreply.github.com>
2025-07-03 06:34:36 +0800

    Feature/appointment module (#6)
    * Modify: Put base setup if need email verification
    
    * Update: Base service complete
    
    * Update: Base setup appointment's services
    
    * Update: Update Module Completed

---
+++ app/Http/Controllers/AppointmentController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Appointment;
+use App\Models\User;
+use App\Models\Pet;
+use App\Models\Service;
+use Illuminate\Http\Request;
+
+class AppointmentController extends Controller
+{
+    public function index()
+    {
+        if (auth()->user()->role == "veterinar") {
+            $appointments = Appointment::all();
+        } else {
+            $appointments = Appointment::all();
+
+            // $appointments = Appointment::where("customer_id", auth()->user()->id)->get();
+        }
+        return view("appointments.index", compact('appointments'));
+    }
+
+    public function addPage()
+    {
+        $pets = Pet::where('owner_id', auth()->user()->id)->get();
+        $services = Service::all();
+        return view("appointments.add", compact("pets", "services"));
+    }
+
+    public function create(Request $request)
+    {
+        // Skip validation for now
+        $appointment = Appointment::create([
+            'customer_id' => $request->customer_id,
+            'pet_id' => $request->pet_id,
+            'staff_id' => $request->staff_id ?? null,
+            'appointment_date' => $request->appointment_date,
+            'appointment_time' => $request->appointment_time,
+            'remarks' => $request->remarks,
+            'status' => $request->status,
+        ]);
+
+        // Attach services if any
+        if ($request->has('services')) {
+            $appointment->service()->sync($request->services);
+        }
+
+        return redirect()->route('appointments')->with('success', 'Appointment added successfully.');
+    }
+
+
+    public function detail($id)
+    {
+        $appointment = Appointment::with('service')->find($id);
+        $pets = Pet::where('owner_id', auth()->user()->id)->get();
+        $services = Service::all();
+        $staffs = User::where('role', 'staff')->get();
+
+        return view("appointments.detail", compact('appointment', 'pets', 'staffs', 'services'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $appointment = Appointment::findOrFail($id);
+
+        $appointment->update([
+            'staff_id' => $request->staff_id,
+            'appointment_date' => $request->appointment_date,
+            'appointment_time' => $request->appointment_time,
+            'remarks' => $request->remarks,
+            'status' => $request->status,
+        ]);
+
+        // Sync services if provided
+        $appointment->service()->sync($request->input('services', []));
+
+        return redirect()->route('appointments')->with('success', 'Appointment updated successfully.');
+    }
+
+
+    public function cancel($id, Request $request)
+    {
+        $appointment = Appointment::find($id);
+        $appointment->update(['status' => 'Cancelled']);
+
+        // Redirect back with success message
+        return redirect()->route(route: 'appointments')->with('success', 'Appointment Cancelled.');
+    }
+
+    public function destroy($id)
+    {
+        $appointment = Appointment::find($id);
+        if ($appointment) {
+            $appointment->delete();
+        }
+        return redirect()->route('appointments')->with('success', 'Appointment deleted successfully');
+    }
+}
---
+++ app/Models/Appointment.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
+class Appointment extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'customer_id',
+        'pet_id',
+        'staff_id',
+        'appointment_date',
+        'appointment_time',
+        'remarks',
+        'status',
+    ];
+
+    protected $casts = [
+        'appointment_date' => 'date',
+        'appointment_time' => 'datetime:H:i',
+    ];
+
+
+    public function customer()
+    {
+        return $this->belongsTo(User::class, 'customer_id');
+    }
+
+    public function pet()
+    {
+        return $this->belongsTo(Pet::class, 'pet_id');
+    }
+
+    public function staff()
+    {
+        return $this->belongsTo(User::class, 'staff_id');
+    }
+
+    public function service()
+    {
+        return $this->belongsToMany(Service::class, 'appointment_services');
+    }
+}
